86f39067d43ac6543cb1431d854d4ee30c316137
Liliana <liliana.monterroza18@gmail.com>
2025-11-06 19:37:48 +0100

    add suma de dos enteros

---
+++ src/SumaEntero.java
@@ -0,0 +1,14 @@
+public class SumaEntero {
+
+            public static void main(String[] args) {
+
+                int num1 = 4;
+                int num2 = 6;
+
+                int resultado = num1 + num2;
+
+                System.out.println("el resultado de la suma es : " + resultado);
+            }
+        }
+
+
